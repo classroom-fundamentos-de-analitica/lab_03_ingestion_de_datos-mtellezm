410f1507e62074132bb010bf5f3d6eac99c74382
mariana <mtellezm@unal.edu.co>
2024-09-16 18:40:58 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -9,13 +9,35 @@ espacio entre palabra y palabra.
 
 
 """
+
 import pandas as pd
 
-
 def ingest_data():
+    with open("clusters_report.txt", "r") as file:
 
-    #
-    # Inserte su código aquí
-    #
+        #Diccionario de las columnas del dataframe
+        data = {"cluster": [], 
+                "cantidad_de_palabras_clave": [], 
+                "porcentaje_de_palabras_clave": [], 
+                "principales_palabras_clave": []
+                }
+        #Cada fila se limpia y cada elemento se separa en una fila
+        for line in file.readlines()[4:]: 
+            line = line.replace("\n", "")
+            data_in_file = list(filter(lambda x: x != "", line.split(" ")))
+
+            #Se va agregando la inforción de cada fila y se diferencia por el %
+            if "%" in data_in_file:
+                data["cluster"].append(int(data_in_file[0]))
+                data["cantidad_de_palabras_clave"].append(int(data_in_file[1]))
+                data["porcentaje_de_palabras_clave"].append(float(data_in_file[2].replace(",", ".")))
+                data["principales_palabras_clave"].append(" ".join(data_in_file[4:]).replace(".", "").strip() + " ")
+            elif data_in_file:
+                data["principales_palabras_clave"][-1] += " ".join(data_in_file).replace(".", "").strip() + " "
+
+        # Eliminar el espacio al final de cada cadena de palabras clave
+        data["principales_palabras_clave"] = list(map(lambda x: x.strip(), data["principales_palabras_clave"]))
+
+        df = pd.DataFrame(data)
 
     return df
